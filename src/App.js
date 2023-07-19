b0df0517f9049c244eb9d94cd78b41bf6903f4f8
ElWask <18539041+ElWask@users.noreply.github.com>
2023-07-19 07:56:22 +0000

    starter interview

--- src/App.js
+++ src/App.js
@@ -1,27 +1,14 @@
 import './App.css';
 
+// Construct a list of markets featuring
+// market name, buy price, sell price & price change 
+// from the following URL:
+// https://a.c-dn.net/b/2Me8Xl.json#data.json
+
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src="Octocat.png" className="App-logo" alt="logo" />
-        <p>
-          GitHub Codespaces <span className="heart">♥️</span> React
-        </p>
-        <p className="small">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </p>
-      </header>
+      
     </div>
   );
 }
