b7024f3fb4eef96f8fb542601334eac4168a38d5
ElWask <18539041+ElWask@users.noreply.github.com>
2023-07-21 09:12:28 +0000

    potential solution

--- src/App.js
+++ src/App.js
@@ -1,14 +1,69 @@
 import './App.css';
+import React, { useEffect, useState } from 'react';
 
-// Construct a list of markets featuring
-// market name, buy price, sell price & price change 
-// from the following URL:
-// https://a.c-dn.net/b/2Me8Xl.json#data.json
+const MARKETS_URL = 'https://a.c-dn.net/b/2Me8Xl.json';
+
+async function processMarketsData(markets) {
+  const groupedMarkets = markets.reduce((accumulator, item) => {
+    accumulator[item.row] = accumulator[item.row] || [];
+    accumulator[item.row][item.col] = item.value;
+    return accumulator;
+  }, []);
+  return groupedMarkets;
+}
+
+function renderRow(row, isHeader) {
+  return row.map((cellValue, index) => {
+    const Element = isHeader ? 'th' : 'td';
+    return <Element key={index}>{cellValue}</Element>;
+  });
+}
 
 function App() {
+  const [rows, setRows] = useState([]);
+  const [error, setError] = useState('');
+
+  async function getMarketsData() {
+    const markets = new Request(MARKETS_URL);
+    const response = await fetch(markets);
+
+    const { data } = await response.json();
+    return data;
+  }
+  
+  useEffect(() => {
+    async function fetchData() {
+      setError('')
+      try {
+        const markets = await getMarketsData();
+        const processedRows = await processMarketsData(markets);
+        setRows(processedRows);
+      } catch (error) {
+        setError('Failed to fetch data');
+      }
+    }
+
+    fetchData();
+  }, []);
+  
+  if (error) {
+    return <div>Error: {error}</div>;
+  }
+
   return (
-    <div className="App">
-      
+    <div>
+      <table id="markets">
+        <thead>
+          <tr>
+            {rows.length > 0 && renderRow(rows[0], true)}
+          </tr>
+        </thead>
+        <tbody>
+          {rows.slice(1).map((row, index) => (
+            <tr key={index}>{renderRow(row, false)}</tr>
+          ))}
+        </tbody>
+      </table>
     </div>
   );
 }
--- src/App.test.js
+++ src/App.test.js
@@ -1,8 +1,48 @@
-import { render, screen } from '@testing-library/react';
+import React from 'react';
+import { render, screen, waitFor } from '@testing-library/react';
+import '@testing-library/jest-dom/extend-expect';
+import fetchMock from 'jest-fetch-mock';
 import App from './App';
 
-test('renders learn react link', () => {
-  render(<App />);
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
+describe('App', () => {
+  beforeAll(() => {
+    fetchMock.resetMocks();
+  });
+
+  it('renders the table header and rows correctly', async () => {
+    // Mock the response data
+    const mockData = [
+      { row: 0, col: 0, value: 'Header 1' },
+      { row: 0, col: 1, value: 'Header 2' },
+      { row: 1, col: 0, value: 'Cell 1' },
+      { row: 1, col: 1, value: 'Cell 2' },
+    ];
+
+    jest.spyOn(global, 'fetch').mockResolvedValue({
+      json: jest.fn().mockResolvedValue({ data: mockData }),
+    });
+
+    render(<App />);
+
+    // Wait for data to be fetched and rendered
+    await waitFor(() => screen.getByText('Header 1'));
+
+    // Check if the table header and rows are rendered correctly
+    expect(screen.getByText('Header 1')).toBeInTheDocument();
+    expect(screen.getByText('Header 2')).toBeInTheDocument();
+    expect(screen.getByText('Cell 1')).toBeInTheDocument();
+    expect(screen.getByText('Cell 2')).toBeInTheDocument();
+  });
+
+  it('handles errors during data fetching', async () => {
+    jest.spyOn(global, 'fetch').mockRejectedValue('something went wrong');
+
+    render(<App />);
+
+    // Wait for the error message to be rendered
+    await waitFor(() => screen.getByText('Error: Failed to fetch data'));
+
+    // Check if the error message is displayed
+    expect(screen.getByText('Error: Failed to fetch data')).toBeInTheDocument();
+  });
 });
