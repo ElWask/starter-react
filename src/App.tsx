2dd62e44cfa6aedb3612b7aced296f4135f1660e
charly <charly@macbook-air-2.home>
2025-08-04 07:27:36 +0100

    Remove non generic title

--- src/App.tsx
+++ src/App.tsx
@@ -2,8 +2,8 @@ function App() {
   return (
     <div className="h-dvh w-dvw bg-gray-200 p-8 flex justify-center">
       <div className="w-1/2">
-        <h1 className="mb-8 text-3xl font-bold text-gray-900 text-center">Market Data</h1>
-        {/* TODO: Add the Table component here */}
+        <h1 className="mb-8 text-3xl font-bold text-gray-900 text-center">Hello World</h1>
+        {/* TODO: Add component here */}
       </div>
     </div>
   );
